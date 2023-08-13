cad974b8e86560d1832d91ad8766f81234ba9ed8
KCA2828 <102121878+KCA2828@users.noreply.github.com>
2023-08-12 19:54:50 -0600

    Respaldos SQL ADMIN.sql
    Se realizaron los respaldos en frio y con EXPDP e IMPDP

--- SQL ADMIN.sql
+++ SQL ADMIN.sql
@@ -2,7 +2,7 @@
 
 -- alter session set "_ORACLE_SCRIPT" = TRUE;
 
--- Creación de Tablespaces
+-- CreaciÃ³n de Tablespaces
 CREATE TABLESPACE TS_PROCEDIMIENTOS 
 DATAFILE 'C:\AdminDB\Procedimientos01.dbf' SIZE 50M AUTOEXTEND ON;
 
@@ -15,7 +15,7 @@ DATAFILE 'C:\AdminDB\Pacientes01.dbf' SIZE 50M AUTOEXTEND ON;
 CREATE TABLESPACE TS_CITAS 
 DATAFILE 'C:\AdminDB\Citas01.dbf' SIZE 100M AUTOEXTEND ON;
 
--- Creación de Tablas
+-- CreaciÃ³n de Tablas
 CREATE TABLE Procedimientos (
    idProcedimiento INTEGER PRIMARY KEY,
    nombre VARCHAR2(50) NOT NULL,
@@ -50,7 +50,7 @@ CREATE TABLE Citas (
    FOREIGN KEY (idProcedimiento) REFERENCES Procedimientos(idProcedimiento)
 ) TABLESPACE TS_CITAS;
 
--- Creación de �?ndices
+-- CreaciÃ³n de Ã?ndices
 CREATE INDEX IDX_NOMBRE_PROCEDIMIENTO ON Procedimientos(nombre) TABLESPACE TS_PROCEDIMIENTOS;
 CREATE INDEX IDX_NOMBRE_DENTISTA ON Dentistas(nombre) TABLESPACE TS_DENTISTAS;
 CREATE INDEX IDX_NOMBRE_PACIENTE ON Pacientes(nombre) TABLESPACE TS_PACIENTES;
@@ -64,51 +64,51 @@ SELECT * FROM Citas;
 
 -- Inserciones en la tabla "Procedimientos"
 INSERT INTO Procedimientos (idProcedimiento, nombre, descripcion, costo)
-VALUES (1, 'Limpieza Dental', 'Remoción de placa y sarro', 80.00);
+VALUES (1, 'Limpieza Dental', 'RemociÃ³n de placa y sarro', 80.00);
 
 INSERT INTO Procedimientos (idProcedimiento, nombre, descripcion, costo)
-VALUES (2, 'Extracción de Muela', 'Extracción de muela dañada', 150.00);
+VALUES (2, 'ExtracciÃ³n de Muela', 'ExtracciÃ³n de muela daÃ±ada', 150.00);
 
 INSERT INTO Procedimientos (idProcedimiento, nombre, descripcion, costo)
 VALUES (3, 'Empaste Dental', 'Rellenar cavidades con material restaurador', 120.00);
 
 INSERT INTO Procedimientos (idProcedimiento, nombre, descripcion, costo)
-VALUES (4, 'Ortodoncia', 'Alineación de dientes y mandíbula', 2000.00);
+VALUES (4, 'Ortodoncia', 'AlineaciÃ³n de dientes y mandÃ­bula', 2000.00);
 
 INSERT INTO Procedimientos (idProcedimiento, nombre, descripcion, costo)
 VALUES (5, 'Blanqueamiento Dental', 'Aclarar el color de los dientes', 250.00);
 
 -- Inserciones en la tabla "Dentistas"
 INSERT INTO Dentistas (idDentista, nombre, apellido, email)
-VALUES (1, 'María', 'Gómez', 'maria@example.com');
+VALUES (1, 'MarÃ­a', 'GÃ³mez', 'maria@example.com');
 
 INSERT INTO Dentistas (idDentista, nombre, apellido, email)
-VALUES (2, 'Juan', 'Rodríguez', 'juan@example.com');
+VALUES (2, 'Juan', 'RodrÃ­guez', 'juan@example.com');
 
 INSERT INTO Dentistas (idDentista, nombre, apellido, email)
-VALUES (3, 'Laura', 'Pérez', 'laura@example.com');
+VALUES (3, 'Laura', 'PÃ©rez', 'laura@example.com');
 
 INSERT INTO Dentistas (idDentista, nombre, apellido, email)
-VALUES (4, 'Carlos', 'Martínez', 'carlos@example.com');
+VALUES (4, 'Carlos', 'MartÃ­nez', 'carlos@example.com');
 
 INSERT INTO Dentistas (idDentista, nombre, apellido, email)
-VALUES (5, 'Ana', 'López', 'ana@example.com');
+VALUES (5, 'Ana', 'LÃ³pez', 'ana@example.com');
 
 -- Inserciones en la tabla "Pacientes"
 INSERT INTO Pacientes (idPaciente, nombre, apellido, email)
-VALUES (1, 'Alejandro', 'Hernández', 'alejandro@example.com');
+VALUES (1, 'Alejandro', 'HernÃ¡ndez', 'alejandro@example.com');
 
 INSERT INTO Pacientes (idPaciente, nombre, apellido, email)
-VALUES (2, 'Isabel', 'Sánchez', 'isabel@example.com');
+VALUES (2, 'Isabel', 'SÃ¡nchez', 'isabel@example.com');
 
 INSERT INTO Pacientes (idPaciente, nombre, apellido, email)
-VALUES (3, 'Roberto', 'García', 'roberto@example.com');
+VALUES (3, 'Roberto', 'GarcÃ­a', 'roberto@example.com');
 
 INSERT INTO Pacientes (idPaciente, nombre, apellido, email)
-VALUES (4, 'Elena', 'Díaz', 'elena@example.com');
+VALUES (4, 'Elena', 'DÃ­az', 'elena@example.com');
 
 INSERT INTO Pacientes (idPaciente, nombre, apellido, email)
-VALUES (5, 'Javier', 'Martín', 'javier@example.com');
+VALUES (5, 'Javier', 'MartÃ­n', 'javier@example.com');
 
 -- Inserciones en la tabla "Citas"
 INSERT INTO Citas (idCita, idPaciente, idDentista, idProcedimiento, fechaCita, motivo, costo)
@@ -118,7 +118,7 @@ INSERT INTO Citas (idCita, idPaciente, idDentista, idProcedimiento, fechaCita, m
 VALUES (2, 2, 2, 3, TO_DATE('2023-08-18', 'YYYY-MM-DD'), 'Empaste dental', 120.00);
 
 INSERT INTO Citas (idCita, idPaciente, idDentista, idProcedimiento, fechaCita, motivo, costo)
-VALUES (3, 3, 3, 2, TO_DATE('2023-08-20', 'YYYY-MM-DD'), 'Extracción de muela', 150.00);
+VALUES (3, 3, 3, 2, TO_DATE('2023-08-20', 'YYYY-MM-DD'), 'ExtracciÃ³n de muela', 150.00);
 
 INSERT INTO Citas (idCita, idPaciente, idDentista, idProcedimiento, fechaCita, motivo, costo)
 VALUES (4, 4, 4, 5, TO_DATE('2023-08-22', 'YYYY-MM-DD'), 'Blanqueamiento dental', 250.00);
@@ -139,25 +139,25 @@ SELECT * FROM Pacientes;
 -- Recuperar todas las citas
 SELECT * FROM Citas;
 
--- Recuperar citas para un paciente espec�fico
+-- Recuperar citas para un paciente específico
 SELECT * FROM Citas WHERE idPaciente = <id_paciente>;
 
--- Recuperar citas para un dentista espec�fico
+-- Recuperar citas para un dentista específico
 SELECT * FROM Citas WHERE idDentista = <id_dentista>;
 
--- Recuperar citas para un procedimiento espec�fico
+-- Recuperar citas para un procedimiento específico
 SELECT * FROM Citas WHERE idProcedimiento = <id_procedimiento>;
 
 -- Recuperar citas dentro de un rango de fechas
 SELECT * FROM Citas WHERE fechaCita BETWEEN TO_DATE('<fecha_inicio>', 'YYYY-MM-DD') AND TO_DATE('<fecha_fin>', 'YYYY-MM-DD');
 
--- Recuperar el costo total de procedimientos para un paciente espec�fico
+-- Recuperar el costo total de procedimientos para un paciente específico
 SELECT idPaciente, SUM(costo) AS costo_total
 FROM Citas
 WHERE idPaciente = <id_paciente>
 GROUP BY idPaciente;
 
--- Recuperar el procedimiento m�s costoso
+-- Recuperar el procedimiento más costoso
 SELECT MAX(costo) AS max_costo
 FROM Procedimientos;
 
@@ -173,6 +173,34 @@ GRANT CONNECT, RESOURCE TO doctor;
 CREATE USER cliente IDENTIFIED BY cliente_password;
 GRANT CONNECT TO cliente;
 
-/*El usuario admin tiene roles CONNECT, RESOURCE y DBA, lo que le da acceso completo a la base de datos y privilegios de administraci�n.
+/*El usuario admin tiene roles CONNECT, RESOURCE y DBA, lo que le da acceso completo a la base de datos y privilegios de administración.
 El usuario doctor tiene roles CONNECT y RESOURCE, lo que le proporciona un acceso general a la base de datos y la capacidad de trabajar con recursos.
 El usuario cliente solo tiene el rol CONNECT, lo que le permite conectarse a la base de datos.*/
+
+-- RESPALDOS FECHA 12/08/2023;
+-- RESPALDOS EN FRIO(CMD)
+C:\Users\kenne>SQL PLUS /NOLOG
+SQL> CONNECT /AS SYSDBA;
+SQL> SHUTDOWN IMMEDIATE
+SQL> EXIT
+C:\Users\kenne>CD \
+C:\>CD martes223
+C:\>CD martes223>DIR   
+C:\martes223>COPY *.DBF C:\BACKUP
+C:\martes223>SQL PLUS /NOLOG
+SQL> CONNECT /AS SYSDBA;   
+SQL> STARTUP
+SQL> EXIT
+-- RESPALDOS CON EXPDP E IMPDP
+SQL> CONNECT /AS SYSDBA;
+SQL>CREATE DIRECTORY BACKUPPROYECTO AS  ‘C:\RESPALDO’;
+SQL>GRANT WRITE ON DIRECTORY BACKUPPROYECTO TO SYSTEM;
+SQL>EXIT
+CREATE TABLE COPIA AS SELECT * FROM Procedimientos;(EN SQL DEVELOPER)
+C:\> EXPDP SYSTEM/root directory=BACKUPPROYECTO dumpfile=SYSTEM.DMP SCHEMAS=SYSTEM
+ GRANT EXP_FULL_DATABASE TO HR;
+ GRANT IMP_FULL_DATABASE TO HR;
+--Borramos la tabla ejemplo en SqlDeveloper con el siguiente comando:
+drop table Procedimientos;
+--Importamos la tabla Procedimientos con el siguiente comando:
+C:\> IMPDP SYSTEM/root directory=BACKUPPROYECTO dumpfile=SYSTEM.DMP SCHEMAS=SYSTEM
